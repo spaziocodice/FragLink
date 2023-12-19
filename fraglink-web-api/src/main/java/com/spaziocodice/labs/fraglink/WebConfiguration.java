5267ec4f5314197e32a0b4e5a8e4936b4383ff87
Andrea Gazzarini <a.gazzarini@spaziocodice.com>
2023-12-19 09:23:17 +0100

    Project refactoring (server + lib)

--- fraglink-web-api/src/main/java/com/spaziocodice/labs/fraglink/WebConfiguration.java
+++ fraglink-web-api/src/main/java/com/spaziocodice/labs/fraglink/WebConfiguration.java
@@ -1,10 +1,12 @@
 package com.spaziocodice.labs.fraglink;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan("com.spaziocodice.labs.fraglink")
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
